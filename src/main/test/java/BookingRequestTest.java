14ebdbf94ee4af4e90fe2a14ff37d51fc866c88c
pdesai2 <pdesai2@ncsu.edu>
2018-06-29 12:52:11 -0400

    unit test classes

---
+++ src/main/test/java/BookingRequestTest.java
@@ -0,0 +1,13 @@
+/**
+ * Created by Poojan on 6/29/2018.
+ */
+public class BookingRequestTest {
+    /*
+    * Test the validate methods and the getrequet methods.
+    * Provide various cases for allowed input and invalid
+    * for get request methods allow scaaner to read fixed input from file
+    * and give one wrong followed by correct input
+    * see the result.
+    * for datetime allow various invalid cases such as 31 february and
+    * see parse expception fail it*/
+}
---
+++ src/main/test/java/ReservationTest.java
@@ -0,0 +1,16 @@
+/**
+ * Created by Poojan on 6/29/2018.
+ */
+public class ReservationTest {
+
+     /*
+    * getUniqueCarIdsForType
+    * getReservationTimesForId
+    * carAvailable
+    * createReservation
+    * all these need to be tested throughly.
+    * Allow mock for database or create a dummy dataset.
+     * Test when tables empty and no data returned,
+     *CAn create IT test for insert into databasse part of reservation in seperate class
+    * */
+}
